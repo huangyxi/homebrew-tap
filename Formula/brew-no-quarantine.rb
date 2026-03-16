b93b73e0dd68accd4629fa4e3512cae6c5c93aa5
Mark Huang <git@hyxi.dev>
2026-03-16 22:15:44 +0800

    Bump formula version to 0.1.1

--- Formula/brew-no-quarantine.rb
+++ Formula/brew-no-quarantine.rb
@@ -1,8 +1,8 @@
 class BrewNoQuarantine < Formula
   desc "Homebrew wrapper to remove the quarantine attribute from casks"
   homepage "https://github.com/huangyxi/brew-no-quarantine"
-  url "https://github.com/huangyxi/brew-no-quarantine/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "5284b86112bfcf5f380b3d7e8b06c0bd41ecb6f033e68a6816dd82154aa3daac"
+  url "https://github.com/huangyxi/brew-no-quarantine/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "a891a60db3bd39396676e7aeaa1471673fc4b8ce3d237e14713f3fbdfe3dbf9c"
   license "MIT"
 
   depends_on :macos
