faf71f9b28b62e25fa0d5f379db4e7ad9252511b
Mark Huang <git@hyxi.dev>
2026-03-16 21:59:47 +0800

    Remove explicit Ruby requirement from Formula

--- Formula/brew-no-quarantine.rb
+++ Formula/brew-no-quarantine.rb
@@ -5,22 +5,12 @@ class BrewNoQuarantine < Formula
   sha256 "5284b86112bfcf5f380b3d7e8b06c0bd41ecb6f033e68a6816dd82154aa3daac"
   license "MIT"
 
-  depends_on "ruby" => :build
   depends_on :macos
 
   def install
-    ENV["BUNDLE_FORCE_RUBY_PLATFORM"] = "1"
-    ENV["BUNDLE_WITHOUT"] = "development test"
-    ENV["BUNDLE_VERSION"] = "system" # Avoid installing Bundler into the keg
-    ENV["GEM_HOME"] = libexec
     ENV["VERSION"] = version
 
-    system "bundle", "install"
-    system "gem", "build", "#{name}.gemspec"
-    system "gem", "install", "#{name}-#{version}.gem"
-
-    bin.install libexec/"bin/#{name}"
-    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    bin.install "bin/#{name}"
 
     bash_completion.install "completions/bash/#{name}"
     fish_completion.install "completions/fish/#{name}.fish"
